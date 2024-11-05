8a0671e3b21e09b31a53999ddcc88b636dfe26b3
Brooke Nelson <brne5584@colorado.edu>
2024-11-04 21:56:49 -0700

    Fixed error in insert.sql with missing column --> this fixed my database

--- ProjectSourceCode/src/init_data/insert.sql
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO users(username, first_name, last_name, password, birthday, sign) VALUES('bob', 'bobby', 'joe', 'test123', '2012-08-05', 'Leo');
-INSERT INTO users(username, first_name, last_name, password, birthday) VALUES('sue', 'susane', 'doe', 'test456', '1995-03-12', 'Pisces');
+INSERT INTO users(username, first_name, last_name, password, birthday, sign) VALUES('sue', 'susane', 'doe', 'test456', '1995-03-12', 'Pisces');
 
 INSERT INTO friendships(user_id, friend_id) VALUES(1, 2);
 INSERT INTO friendships(user_id, friend_id) VALUES(2, 1);
