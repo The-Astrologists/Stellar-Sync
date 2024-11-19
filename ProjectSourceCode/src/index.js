f9f547ecb9c46c24874c357ab02d2432b3e92ec2
Katie Miller <katiemill18@icloud.com>
2024-11-19 13:51:34 -0700

    added friends to navbar

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -412,7 +412,7 @@ app.get('/search', async (req, res) => {
 
 ///// friends /////
 app.get('/friends', (req, res) => {
-  res.render('pages/friends',{
+  res.render('pages/friends', {
     username: req.session.username,
   });
 });
