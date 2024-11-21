9aff9dcdc1d580ad3dcd6004b95279ac99490ecf
Brooke Seifert <brse2921@colorado.edu>
2024-11-21 16:29:16 -0700

    songs

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -34,7 +34,9 @@ const hbs = handlebars.create({
 
 // database configuration
 const dbConfig = {
-  host: process.env.POSTGRES_HOST, // the database server
+  // host: process.env.POSTGRES_HOST, // the database server
+  host: 'db', // the database server
+  
   port: process.env.POSTGRES_PORT, // the database port
   database: process.env.POSTGRES_DB, // the database name
   user: process.env.POSTGRES_USER, // the user account to connect with
