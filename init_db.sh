f5c496eabc11f06a4cdb48a597e6419db954c829
Sam Kutcipal <samkuts@comcast.net>
2024-11-21 12:33:32 -0700

    yet another db link

--- init_db.sh
+++ init_db.sh
@@ -4,7 +4,7 @@
 # This file contains sensitive information and should be kept private
 
 # TODO: Set your PostgreSQL URI - Use the External Database URL from the Render dashboard
-PG_URI="postgresql://user:A0SRsnhsZT9rSKXD19WCWFmsRIjSqWQj@dpg-csvods52ng1s73du7ge0-a.oregon-postgres.render.com/users_db_q7yl"
+PG_URI="postgresql://user:9ZRXVw6sNrLXbi7cpPqZbVYhgKGYtyy8@dpg-csvol39opnds73ei4dng-a.oregon-postgres.render.com/users_db_i3d4"
 
 # Execute each .sql file in the directory
 for file in init_data/*.sql; do
