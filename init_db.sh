17912656ac2ad8018389a511c9c51cf34f5a9a1d
Brooke Seifert <brse2921@colorado.edu>
2024-11-21 14:31:06 -0700

    changed init shell file

--- init_db.sh
+++ init_db.sh
@@ -4,7 +4,7 @@
 # This file contains sensitive information and should be kept private
 
 # TODO: Set your PostgreSQL URI - Use the External Database URL from the Render dashboard
-PG_URI="postgresql://users_db_soou_user:Iav17m7yaKvfLGdTxVSni1EwD2bwoaGT@dpg-csvmds0gph6c73e25730-a.oregon-postgres.render.com/users_db_soou"
+PG_URI="postgresql://user:7NeRKiSpJaY6UvgQPouDr3mB0xpTTZIL@dpg-csvq6452ng1s73ea3oq0-a.oregon-postgres.render.com/user_db_4azi"
 
 # Execute each .sql file in the directory
 for file in init_data/*.sql; do
